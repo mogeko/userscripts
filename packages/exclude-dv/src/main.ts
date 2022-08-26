c116281d685348289b70d2062a9ad1b5c9152c67
Mogeko <zhengjunyi@live.com>
2022-08-26 17:47:30 +0200

    feat: migration exclude-dv

---
+++ packages/exclude-dv/src/main.ts
@@ -0,0 +1,10 @@
+const HDR = "https://dyncdn.me/static/20/images/categories/cat_new52.gif";
+
+document.querySelectorAll("table.lista2t tr.lista2").forEach((tr) => {
+  const link = tr.querySelector<HTMLLinkElement>("td:nth-child(2) > a");
+  const cover = tr.querySelector<HTMLImageElement>("td:nth-child(1) img");
+
+  if (link?.title?.includes(".DV.") && cover?.src === HDR) tr.remove();
+});
+
+export {};

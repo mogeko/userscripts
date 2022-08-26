a9afb27dd4076ae3d430345faddb86dfc34fb404
Mogeko <zhengjunyi@live.com>
2022-08-26 17:59:50 +0200

    feat: Migration template

---
+++ dist/template.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        New Script
+// @description There is a template to create UserScript.
+// @namespace   npmjs.com/rollup-plugin-userscript-metablock
+// @author      Mogeko
+// @license     MIT
+// @version     0.0.1
+// @grant       none
+// ==/UserScript==
+(function () {
+	'use strict';
+
+	console.log("Hello World");
+
+})();

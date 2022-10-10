3c44af45046bfbda3dfdcbd71f9e6dfe1c04dc65
Mogeko <zhengjunyi@live.com>
2022-10-10 18:04:43 +0200

    build: beautiful formatting output meta info

--- scripts/meta.js
+++ scripts/meta.js
@@ -8,24 +8,28 @@ async function main() {
   const release_files = await fs.readdir(release_path);
   const base_url = argv.base_url ?? "https://mogeko.github.io/userscripts";
 
+  const meta = {
+    name: pkg.name,
+    description: pkg.description,
+    homepage: pkg.homepage,
+    author: pkg.author,
+    license: pkg.license,
+    resource: release_files.map((file) => `${base_url}/${file}`),
+    packer: "https://www.npmjs.com/package/rollup",
+    env: {
+      NODE_VERSION: process.version,
+      RUNNER_OS: process.env.RUNNER_OS,
+      RUNNER_ARCH: process.env.RUNNER_ARCH,
+    },
+    date: new Date(Date.now()).toISOString(),
+  };
+
   fs.writeFile(
     path.resolve(__dirname, "../release/index.json"),
-    JSON.stringify({
-      name: pkg.name,
-      description: pkg.description,
-      homepage: pkg.homepage,
-      author: pkg.author,
-      license: pkg.license,
-      resource: release_files.map((file) => `${base_url}/${file}`),
-      packer: "https://www.npmjs.com/package/rollup",
-      env: {
-        NODE_VERSION: process.version,
-        RUNNER_OS: process.env.RUNNER_OS,
-        RUNNER_ARCH: process.env.RUNNER_ARCH,
-      },
-      date: new Date(Date.now()).toISOString(),
-    })
+    JSON.stringify(meta, null, 2)
   );
+
+  return meta;
 }
 
 main();

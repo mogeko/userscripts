5ccbd400559accd37a3af05f98c6af501a867142
Mogeko <zhengjunyi@live.com>
2022-11-02 16:56:41 +0100

    docs: add API reference docs for build secipt

--- scripts/build.js
+++ scripts/build.js
@@ -3,15 +3,35 @@ import swc from "rollup-plugin-swc";
 import path from "path";
 
 /**
+ * a script to build the userscript.
  *
  * @typedef {Parameters<swc>[0]} SWCConfig
  * @typedef {Parameters<metablock>[0]} MetablockConfig
+ *
  * @param {{mainFile: string, outFile: string,
- *          swcConfig: SWCConfig, metablockConfig: MetablockConfig}} opt
- * @returns {{input: string,
- *  output: {file: string, format: string},
- *  plugins: [ReturnType<swc>, ReturnType<metablock>]
- * }}
+ *          swcConfig: SWCConfig, metablockConfig: MetablockConfig}}
+ *        opt - options.
+ * @returns {{input: string, output: {file: string, format: string},
+ *            plugins: [ReturnType<swc>, ReturnType<metablock>]}}
+ *          the rollup config.
+ *
+ * @remarks
+ * This script is used to build the userscript.
+ *
+ * Among them, we use two rollup plug-ins: `rollup-plugin-swc`
+ * and `rollup-plugin-userscript-metablock` to configure SWC
+ * and userscript meta respectively.
+ *
+ * Usually, you only need `import` and `export rollupBuild()`.
+ *
+ * @see https://rollupjs.org/guide/en/#big-list-of-options
+ *
+ * @example
+ * ```typescript
+ * import rollupBuild from "../../scripts/build";
+ *
+ * export default rollupBuild();
+ * ```
  */
 function rollupBuild(opt = {}) {
   const pkg = require(`${path.resolve(__dirname)}/package.json`);
